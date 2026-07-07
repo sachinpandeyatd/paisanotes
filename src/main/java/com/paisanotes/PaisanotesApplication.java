6ef2b13905a010972194969c1c80e31f551075cb
Sachin Pandey <33sachinedu@gmail.com>
2026-07-07 23:10:45 +0530

    Create the paisanotes spring app

---
+++ src/main/java/com/paisanotes/PaisanotesApplication.java
@@ -0,0 +1,13 @@
+package com.paisanotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisanotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisanotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paisanotes/PaisanotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paisanotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisanotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
